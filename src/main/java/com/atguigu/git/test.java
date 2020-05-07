6c2ed05ee754092c12f295f454af9f62c101f5e4
wangchao <wangchao@163.com>
2020-05-07 10:42:42 +0800

    test idea测试提交 加入输出日志，删除main方法

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -1,15 +1,14 @@
 package com.atguigu.git;
 
+import java.io.Serializable;
+
 /**
  * @description:
  * @author: ALee
  * @create: 2020-05-07 09:32:53
  **/
-public class test implements Serializable{
+public class test implements Serializable {
 
-	public static void main(String[] args) {
-		System.out.println("����");
-	}
     public static void main(String[] args) {
         System.out.println("测试dsa");
     }
